60350129b855ae5e0458c4fc888ae50c74555e70
Gen Taliaru <gen@infira.ee>
2020-06-09 16:33:58 +0300

    improved callback

--- src/__generalUtils.php
+++ src/__generalUtils.php
@@ -31,10 +31,10 @@ function isAjaxRequest()
 {
 	if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
 	{
-		return TRUE;
+		return true;
 	}
 	
-	return FALSE;
+	return false;
 }
 
 
@@ -77,7 +77,7 @@ function getUserIP()
 	return $ipaddress;
 }
 
-function explodeJoin($explodeDelimiter, $str, $joinDelimiter, $joinKeys = FALSE)
+function explodeJoin($explodeDelimiter, $str, $joinDelimiter, $joinKeys = false)
 {
 	$ex       = explode($explodeDelimiter, $str);
 	$joinKeys = \Infira\Utils\Variable::toArray($joinKeys);
@@ -104,7 +104,7 @@ function explodeJoin($explodeDelimiter, $str, $joinDelimiter, $joinKeys = FALSE)
 	return join($joinDelimiter, $data);
 }
 
-function defaultValue($value, $defaultValue, $defaultOn = FALSE)
+function defaultValue($value, $defaultValue, $defaultOn = false)
 {
 	if ($value === $defaultOn)
 	{
@@ -146,29 +146,51 @@ function is_closure($t)
 	return is_object($t) && ($t instanceof Closure);
 }
 
-function callback($callback, $scope = FALSE, $params = [])
+function callback($callback, $scope = false, $params = [])
 {
-	if ($scope === FALSE)
+	if (is_string($callback))
 	{
+		$callStatic = false;
+		if (strpos($callback, '::') !== false)
+		{
+			$ex         = explode('::', $callback);
+			$callStatic = true;
+			$scope      = trim($ex[0]);
+			$callback   = trim($ex[1]);
+		}
+		if ($scope)
+		{
+			if ($callStatic)
+			{
+				return $scope::$callback(...$params);
+			}
+			else
+			{
+				return $scope->$callback(...$params);
+			}
+		}
+		else
+		{
+			return call_user_func_array($callback, $params);
+		}
+	}
+	elseif (is_closure($callback))
+	{
+		if ($scope)
+		{
+			if (is_string($scope))
+			{
+				$scope = new $scope();
+			}
+			$callback = $callback->bindTo($scope);
+		}
+		
 		return call_user_func_array($callback, $params);
 	}
 	else
 	{
-		if (is_string($callback))
-		{
-			return call_user_func_array([$scope, $callback], $params);
-		}
-		elseif (is_closure($callback))
-		{
-			$callback = $callback->bindTo($scope);
-			
-			return call_user_func_array($callback, $params);
-		}
-		else
-		{
-			addExtraErrorInfo("callback", gettype($callback));
-			alert("unimplemented callback paramaterer");
-		}
+		addExtraErrorInfo("callback", gettype($callback));
+		alert("unimplemented callback paramaterer");
 	}
 }
 
@@ -178,20 +200,21 @@ function checkArray($array)
 	{
 		if (count($array) > 0)
 		{
-			return TRUE;
+			return true;
 		}
 	}
-	return FALSE;
+	
+	return false;
 }
 
-$GLOBALS["__globalFlags__"] = FALSE;
-function flag($name = NULL, $value = NULL)
+$GLOBALS["__globalFlags__"] = false;
+function flag($name = null, $value = null)
 {
-	if ($name !== NULL AND $value === NULL)
+	if ($name !== null and $value === null)
 	{
 		if (!isset($GLOBALS["__globalFlags__"][$name]))
 		{
-			return FALSE;
+			return false;
 		}
 	}
 	else
@@ -213,10 +236,10 @@ function pre($var)
  * @param mixed        $variable - can be all kind of type
  * @param unknown_type $trace
  */
-$GLOBALS["debugIsActive"] = FALSE;
+$GLOBALS["debugIsActive"] = false;
 function debug()
 {
-	$GLOBALS["debugIsActive"] = TRUE;
+	$GLOBALS["debugIsActive"] = true;
 	$args                     = func_get_args();
 	$html                     = "";
 	if (isAjaxRequest())
@@ -251,7 +274,7 @@ function debug()
 		exit();
 	}
 	
-	return TRUE;
+	return true;
 	
 }
 
@@ -272,7 +295,7 @@ function debugCollection()
 	debug($GLOBALS["debugCollection"]);
 }
 
-function cleanOutput($isRecursive = FALSE)
+function cleanOutput($isRecursive = false)
 {
 	if (ob_get_contents())
 	{
@@ -280,25 +303,25 @@ function cleanOutput($isRecursive = FALSE)
 		ob_end_clean();
 		if ($isRecursive)
 		{
-			cleanOutput(TRUE);
+			cleanOutput(true);
 		}
 	}
 }
 
 
-function debugClean($v1, $v2 = NULL)
+function debugClean($v1, $v2 = null)
 {
 	$args = func_get_args();
 	debug((count($args) == 1) ? $args[0] : $args);
 }
 
 
-function dump($variable, $echo = FALSE)
+function dump($variable, $echo = false)
 {
 	
 	if (is_array($variable) or is_object($variable))
 	{
-		$html = print_r($variable, TRUE);
+		$html = print_r($variable, true);
 	}
 	else
 	{
@@ -306,7 +329,7 @@ function dump($variable, $echo = FALSE)
 		var_dump($variable);
 		$html = ob_get_clean();
 	}
-	if ($echo == TRUE)
+	if ($echo == true)
 	{
 		exit($html);
 	}
@@ -335,20 +358,20 @@ function getTrace($startAt = 0)
 }
 
 
-function trace($title = FALSE)
+function trace($title = false)
 {
 	if (!empty($title))
 	{
 		echo "----------TRACE : $title --------------" . BR;
 	}
-	echo getTrace(TRUE);
+	echo getTrace(true);
 }
 
 function isMobile()
 {
 	if (!isset($_SERVER['HTTP_USER_AGENT']))
 	{
-		return FALSE;
+		return false;
 	}
 	
 	return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER['HTTP_USER_AGENT']) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
